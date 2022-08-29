f00a5017f96b4f5c046cf64c2d7404b562b591ef
Miguel Ripoll <miguel.ripoll@outlook.com>
2022-08-29 22:59:04 +0200

    Bump std version

--- deps.ts
+++ deps.ts
@@ -5,4 +5,4 @@ export { Select } from "https://deno.land/x/cliffy@v0.24.3/prompt/select.ts";
 export { Input } from "https://deno.land/x/cliffy@v0.24.3/prompt/mod.ts";
 export { Confirm } from "https://deno.land/x/cliffy@v0.24.3/prompt/confirm.ts";
 
-export * as semver from "https://deno.land/std@0.151.0/semver/mod.ts";
+export * as semver from "https://deno.land/std@0.153.0/semver/mod.ts";
